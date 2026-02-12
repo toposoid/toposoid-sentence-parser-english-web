1f1ff6222b656075e89e88c71fe67b356fd13187
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2026-02-12 08:14:42 +0000

    https://github.com/toposoid/toposoid-sentence-parser-english-web/issues/121

--- SentenceParser.py
+++ SentenceParser.py
@@ -99,7 +99,8 @@ class SentenceParser():
                 rangeExpressions = rangeExp,
                 categories = {},
                 domains = {},
-                knowledgeFeatureReferences = []
+                knowledgeFeatureReferences = [],
+                properNouns = {}
             )
             
             predicateArgumentStructure = PredicateArgumentStructure(
@@ -117,7 +118,10 @@ class SentenceParser():
                 modalityType =  "-",
                 parallelType = "-",
                 nodeType = nodeType,
-                morphemes = [token.pos_]
+                morphemes = [token.pos_],
+                caseGroupType = 0, #TODO enumを共通に持つ
+                casePhraseId = "",
+                casePhrase = ""
             )
 
             node = KnowledgeBaseNode(
@@ -155,13 +159,20 @@ class SentenceParser():
 
     #Get named entity and quantity range representation from words starting with a character index.
     def extractNerAndRange(self, beginIndex, nerInfo):
-        nerExpression = ""
+        nerExpression = {}
         rangeExpression = {"": {}}
         if len(nerInfo) == 0: return (nerExpression, rangeExpression)
 
         hitInfo = list(filter(lambda x: x["begin"] <= beginIndex and x['end'] >= beginIndex, nerInfo))
+        
+        for element in hitInfo:
+            nerExpression[element["word"]] = element["ner"]
+            rangeExpression = {element["word"]:{"quantity":element["quantity"], "unit":element["unit"], "range":element["range"],"prefix":element["prefix"] }}
+        
+        """
         if len(hitInfo) != 0:
             nerExpression = hitInfo[0]["ner"]
             rangeExpression = {hitInfo[0]["word"]:{"quantity":hitInfo[0]["quantity"], "unit":hitInfo[0]["unit"], "range":hitInfo[0]["range"],"prefix":hitInfo[0]["prefix"] }}        
+        """
         return (nerExpression, rangeExpression)
 
